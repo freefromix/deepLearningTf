bc182c1481abf0f91bf01f49b1cc20ea981bb1c4
Julien Giraud <freefromix@users.noreply.github.com>
2019-06-21 21:36:11 -0300

    second commit

---
+++ code/1_gan/.ipynb_checkpoints/gan-checkpoint.py
@@ -0,0 +1,12 @@
+"""Gan tutorial"""
+
+# https://www.oreilly.com/learning/generative-adversarial-networks-for-beginners
+
+import tensorflow as tf
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+
+%matplotlib inline
+
+from tensorflow.
